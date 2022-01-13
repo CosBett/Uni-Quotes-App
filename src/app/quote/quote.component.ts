999468f02743d1d4595835965b13ecc15ef68ca5
CosBett <cosmasbett9@gmail.com>
2022-01-13 13:10:24 +0300

    add array to QuoteComponent

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  @Output() isDelete = new EventEmitter<boolean>();
+  quotes: Quote[] = [
+    new Quote('A user interface is like a joke, if you have to explain it, it is not that good ', 'Martin LeBlare', 'Cosmas Bett', new Date(2021, 12, 19), 0, 0),
+    new Quote('The cost of being wrong is less than the cost of doing nothing ', 'Seth Godin', 'Cosmas Bett', new Date(2021, 12, 20), 0, 0),
+    new Quote('Have no fear of perfection.you will never reach it ', 'Salvador Dali', 'Cosmas Bett', new Date(2021, 12, 15), 0, 0),
+    new Quote('The most damaging phrase in the languange is .. it is always been done this way', 'Grace Hopper', 'Cosmas Bett', new Date(2021, 12, 28), 0, 0),
+    new Quote('Programing is not about what you know ; it is about what you can figure out', 'Chris Pine', 'Cosmas Bett', new Date(2022, 1, 1), 0, 0),
+  ]
   constructor() { }
 
   ngOnInit(): void {

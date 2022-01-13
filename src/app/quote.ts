4df0c4aac4367698eb9b8642550a71e4a54df89d
CosBett <cosmasbett9@gmail.com>
2022-01-13 13:02:21 +0300

    update class component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,17 @@
 export class Quote {
+  public showDetails: boolean;
+  public showButton: boolean;
+  public votes: number;
+  constructor(
+    public description: string,
+    public author: string,
+    public postedBy: string,
+    public timePosted: Date,
+    public upVote: number,
+    public downVote: number) {
+
+    this.showDetails = false;
+    this.showButton = true;
+    this.votes = 0;
+  }
 }

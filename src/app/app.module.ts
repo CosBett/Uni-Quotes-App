e3599d0b9052638e0751000eb672331fee4adf59
CosBett <cosmasbett9@gmail.com>
2022-01-13 13:23:56 +0300

    generate DeleteComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormComponent } from './form/form.component';
     NavbarComponent,
     LandingPageComponent,
     QuoteComponent,
-    FormComponent
+    FormComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
